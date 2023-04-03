2c0f81005348fb39aa5905469592e7cb8f9d9478
krunalthakare17 <krunalthakare17@gmail.com>
2023-04-03 22:03:54 +0530

    GPT-3__reactjs_webapp

--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -3,12 +3,23 @@ import Feature from '../../components/feature/Feature';
 import './Features.css'
 
 const FeaturesItem = [
-  { title: 'Improvinf and distrusts intantly', text: 'gbvvffdyyyyyyyyyyyyyyyyyyy' },
-  { title: 'gfyyyyyyyytfff', text: 'regergwgre' },
-  { title: 'Improvinf and distrusts intantly', text: 'ddddddddddh' },
-  { title: 'Improvinf and distrusts intantly', text: 'rggg34g' },
-]
-
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
+  },
+];
 function Features() {
   return (
     <div className="features">
@@ -18,7 +29,7 @@ function Features() {
       </div>
       <div className="features_container">
         {FeaturesItem.map((item, index) => (
-          <Feature title={item.text} text={item.text} key={item.title + index}></Feature>
+          <Feature title={item.title} text={item.text} key={item.title + index}></Feature>
         ))};
       </div>
     </div>
